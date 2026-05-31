87a39ee042b27c036630c20c2a694ee6b007e996
otzua <sglaxy936@gmail.com>
2026-05-31 18:01:38 +0530

    chore: purge junk files, scratchpads, and IDE configs from github repository

--- patch_proxy_video.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-
-const filePath = '/Users/otzua/CODE/REIATSU/anime-api/api/beyond.js';
-let content = fs.readFileSync(filePath, 'utf8');
-
-content = content.replace(/if \\(acceptRanges\\) c\\.header\\('Accept-Ranges', acceptRanges\\);/g, `c.header('Accept-Ranges', 'bytes');`);
-
-fs.writeFileSync(filePath, content, 'utf8');
